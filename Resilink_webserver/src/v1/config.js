c9959b3a5660a7b73bf241f2f253bf9b45bd02c3
ZiQuwi <axelcaz@hotmail.fr>
2025-02-20 09:45:55 +0100

    working version

--- Resilink_webserver/src/v1/config.js
+++ Resilink_webserver/src/v1/config.js
@@ -6,6 +6,8 @@ const SWAGGER_URL = process.env.SWAGGER_URL;
 
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 
+const TOKEN_KEY = process.env.TOKEN_KEY;
+
 const DB_URL = process.env.DB_URL;
 
 const DB_LOGS_URL = process.env.DB_LOGS_URL;
@@ -31,6 +33,7 @@ module.exports = {
     PORT,
     SWAGGER_URL,
     ENCRYPTION_KEY,
+    TOKEN_KEY,
     DB_URL,
     DB_LOGS_URL,
     PATH_ODEP_USER,
